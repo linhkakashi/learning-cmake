28b45052258a6a495b2ba9f49f91401649820a3f
linhtn <hotdog160993@gmail.com>
2019-04-15 16:09:11 +0700

    Finish Step 2

--- cmake-tutorial/tutorial.cxx
+++ cmake-tutorial/tutorial.cxx
@@ -1,21 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 #include "TutorialConfig.h"
+
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
+
 int main (int argc, char *argv[])
 {
   if (argc < 2)
     {
     fprintf(stdout,"%s Version %d.%d\n",
-        argv[0],
-        VERSION_MAJOR,
-        VERSION_MINOR);
+            argv[0],
+            VERSION_MAJOR,
+            VERSION_MINOR);
     fprintf(stdout,"Usage: %s number\n",argv[0]);
     return 1;
     }
+
   double inputValue = atof(argv[1]);
-  double outputValue = sqrt(inputValue);
+  double outputValue = 0;
+
+  if(inputValue >= 0)
+    {
+#ifdef USE_MYMATH
+    outputValue = mysqrt(inputValue);
+#else
+    outputValue = sqrt(inputValue);
+#endif
+    }
+
   fprintf(stdout,"The square root of %g is %g\n",
           inputValue, outputValue);
   return 0;
